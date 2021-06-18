5c26899d6f994e91dd6fe7e50ea8708eb9dc04db
Joel Daros <joel.daros@gmail.com>
2021-06-18 16:33:01 -0300

    refatoracao do codigo

--- ali.py
+++ ali.py
@@ -1,18 +1,51 @@
 import pyautogui
 from pyautogui import locateOnScreen as LoS
 from time import sleep
+import numpy
+import random
+import winsound
+from datetime import datetime
 
-sleep(10)
+cupons = numpy.array([
+    'superdicas20', 'chinacupon20', 'alitec20', 'importchina20', '2021ali20',
+    'sav20', 'alipay20', 'awintop20', 'cupomvalido20'
+])
+
+# dados para emitir o beep quando aplicar o cumpom. 
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+
+pyautogui.FAILSAFE = 0
+
+sleep(2)
+# capturando coordenadas do campo codigo promocional
+x, y = pyautogui.locateCenterOnScreen('codigo_promo.png')
+
+# capturando coordenadas do campo botão utilizar
+a, b = pyautogui.locateCenterOnScreen('utilizar.png')
+
+c, d = pyautogui.locateCenterOnScreen('fazer.png')
+
 while True:
-    TOTAL = LoS('total.png', confidence =0.8)
+    # enquanto valor total não mudar
+    TOTAL = LoS('total.png', confidence=0.8)
+    print('Coordenadas do Total: ', TOTAL)
+
     if TOTAL:
-        sleep(2)
-        x, y = pyautogui.locateCenterOnScreen('text.png', confidence =0.8)
+        print(datetime.now(),'clicar no campo do codigo promocional')
         pyautogui.click(x, y)
-        pyautogui.write('cupomvalido20')
+        # pyautogui.click('capture.png')
+
+        pyautogui.write(cupons[random.randint(1, len(cupons)-1)])
+
+        print(datetime.now(),'clicar no botao utilizar')
+        pyautogui.click(a, b)
         sleep(1)
-        z, t = pyautogui.locateCenterOnScreen('utilizar.png', confidence =0.8)
-        pyautogui.click(z, t)
     else:
+        # se o total mudar e por que consiguiu aplicar o cupon, clicar em fazer o pedido
+        print(datetime.now(),'valor total mudou, fazer o pedido')
+        winsound.Beep(frequency, duration)
+        pyautogui.click(c, d)
+        print(datetime.now(),'pedido finalizado')
+        winsound.Beep(frequency, duration)
         break
-       
